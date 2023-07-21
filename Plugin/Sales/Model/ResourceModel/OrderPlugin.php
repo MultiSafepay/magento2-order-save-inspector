9199b9699feeba3f5f01cfc560f75d7c9083eb23
Vinod Sowdagar <vinod.sowdagar@multisafepay.com>
2023-07-21 18:32:46 +0200

    Add disclaimer and change copyright text

--- Plugin/Sales/Model/ResourceModel/OrderPlugin.php
+++ Plugin/Sales/Model/ResourceModel/OrderPlugin.php
@@ -1,16 +1,13 @@
 <?php
 /**
- *
  * NOTICE OF LICENSE
- *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is provided with Magento in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
  *
- * Copyright © 2022 MultiSafepay, Inc. All rights reserved.
+ * Copyright © MultiSafepay, Inc. All rights reserved.
  * See DISCLAIMER.md for disclaimer details.
- *
  */
 
 declare(strict_types=1);
